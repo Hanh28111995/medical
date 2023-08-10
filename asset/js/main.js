8874f93b2cbd4d83aacd8beeaf2226e3a931b6c9
hanhT <gm.tranhuuhanh@gmail.com>
2023-08-10 16:30:16 -0700

    fix bug 1

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,23 @@
 //// active galery btn ////
 
-function avtive_list (x) {
+function avtive_list(x) {
     let deactive = document.querySelectorAll('.gallery-part .btn-list .fltr-controls')
-    for (let i = 0; i < deactive.length ; i++)
-    {
+    for (let i = 0; i < deactive.length; i++) {
         deactive[i].classList.remove('active')
     }
     document.querySelector('.fltr-controls[data-filter="' + `${x}` + '"]').classList.add('active');
 };
+
+//////spin loading video
+
+var video = document.querySelectorAll('.video');
+for (let i = 0; i < video.length; i++) {
+
+
+
+    video[i].addEventListener('loadeddata', function () {
+        document.querySelectorAll('.spinner')[i].style.display = "none";
+    }
+        , false);
+}
+
