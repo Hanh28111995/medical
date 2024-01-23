9b678df60120f8de030de8f1641067bb0c8fca2e
hanhT <gm.tranhuuhanh@gmail.com>
2024-01-22 16:37:36 -0800

    replace video tag

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,15 +9,33 @@ function avtive_list(x) {
 };
 
 //////spin loading video
-
 var video = document.querySelectorAll('.video');
 for (let i = 0; i < video.length; i++) {
-
-
-
     video[i].addEventListener('loadeddata', function () {
         document.querySelectorAll('.spinner')[i].style.display = "none";
     }
         , false);
 }
+
+
+// $(document).ready(function(){
+//     $('.counter').countUp();    
+// })
+
+$(window).on('load', function () {
+            const filterizr = $('.filter-container').filterizr({
+                controlsSelector: '.fltr-controls',
+                gridItemsSelector: '.filtr-item',
+                spinner: {
+                    enabled: true,
+                },
+            });
+            $("#shuffle").click(() => {
+                filterizr.filterizr('shuffle');
+            })
+            const sortFilterizr = (order) => {
+                filterizr.filterizr('sort', $("select :selected").val(), order);
+            }
+        })
+
 
