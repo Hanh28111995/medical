55b41db393afc8c246ce0e65e9398f77fb837542
hanhT <gm.tranhuuhanh@gmail.com>
2024-01-23 16:44:09 -0800

    fix video

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,33 +9,57 @@ function avtive_list(x) {
 };
 
 //////spin loading video
-var video = document.querySelectorAll('.video');
-for (let i = 0; i < video.length; i++) {
-    video[i].addEventListener('loadeddata', function () {
-        document.querySelectorAll('.spinner')[i].style.display = "none";
+// var video = document.querySelectorAll('.video');
+// for (let i = 0; i < video.length; i++) {
+//     video[i].addEventListener('loadeddata', function () {
+//         document.querySelectorAll('.spinner')[i].style.display = "none";
+//     }
+//         , false);
+// }
+
+let list = document.querySelectorAll('.youtube-index')
+
+for (let i = 0; i < list.length; i++) {    
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player(list[i].id, {
+            events: {
+                'onReady': onPlayerReady
+            }
+        });
     }
-        , false);
+    function onPlayerReady(event) {
+        document.getElementById('ScrollAndRun').addEventListener('click', function () {
+            if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+                player.pauseVideo();
+            } else {
+                player.playVideo();
+            }
+        });
+    }
 }
 
 
-// $(document).ready(function(){
-//     $('.counter').countUp();    
-// })
+
+
+$(document).ready(function(){
+    $('.counter').countUp();    
+})
 
 $(window).on('load', function () {
-            const filterizr = $('.filter-container').filterizr({
-                controlsSelector: '.fltr-controls',
-                gridItemsSelector: '.filtr-item',
-                spinner: {
-                    enabled: true,
-                },
-            });
-            $("#shuffle").click(() => {
-                filterizr.filterizr('shuffle');
-            })
-            const sortFilterizr = (order) => {
-                filterizr.filterizr('sort', $("select :selected").val(), order);
-            }
-        })
+    const filterizr = $('.filter-container').filterizr({
+        controlsSelector: '.fltr-controls',
+        gridItemsSelector: '.filtr-item',
+        spinner: {
+            enabled: true,
+        },
+    });
+    $("#shuffle").click(() => {
+        filterizr.filterizr('shuffle');
+    })
+    const sortFilterizr = (order) => {
+        filterizr.filterizr('sort', $("select :selected").val(), order);
+    }
+})
 
 
